053e190d56d501d8e0a0ab844eb4862ab2c8d05f
Alex Kyllo <alex.kyllo@gmail.com>
2021-03-08 13:06:04 -0800

    start perf main file

---
+++ src/soft_dtw_perf_main.cpp
@@ -0,0 +1,14 @@
+/** Main file for running performance experiments on test data
+ *  @file soft_dtw_perf_main.cpp
+ */
+
+int main()
+{
+    // TODO
+    // Read in one or more input time series files (delimited format) into
+    // arrays and allocate / copy to device
+    // Launch function to calculate all soft-dtw pairwise distances and
+    // time execution
+    // Repeat for multiple input dataset sizes and univariate vs. multivariate
+    return 0;
+}
