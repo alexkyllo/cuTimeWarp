e2514d09e614694e3b9785a40136c978029aeb31
Afrooz Rahmati <afrooz@uw1-320-01p.uwb.edu>
2021-03-13 13:31:07 -0800

    Document Update for Wavefront

--- src/kernels/soft_dtw_tiled.cu
+++ src/kernels/soft_dtw_tiled.cu
@@ -1,19 +1,10 @@
 #include "soft_dtw_tiled.cuh"
 #include "helper_functions.cuh"
 
+/*credit and appreciation to Mehmet E. Belviranli
+https://mehmet.belviranli.com/papers/ics15.pdf
+*/
 
-// __global__ void softdtw_tiled_kernel(float *D, float *R, float *cost, uint m,
-//                                      uint n, float gamma)
-// {
-//     // TODO
-//     // Divide R into tiles
-//     // Each tile depends on the tiles to its top, left, and top-left
-//     // Assign one thread to spin on the signal variable for this tile
-//     // Process the tile diagonally from upper left to lower right
-//     // using a loop counter to keep track of fully processed diagonals
-//     // and while loop and syncthreads to spin on it
-//     // Write to the signal variables to signal the next tiles
-// }
 
 /** Kernel function for computing tiled Soft DTW using wave front approach
  * matrix for multivariate time series with CUDA. Input a , b and D should be a
@@ -24,16 +15,11 @@
  * @param n Length of second time series
  * @param D The pairwise distance array of two time series
  * @param waveId the ID for the wave
- * @param total_tiles_rows
- * @param total_tiles_columns
- * @param tile_width
- * @param tileRow
- * @param tileColumn
+ * @param total_tiles_rows The total number of tiles within the row
+ * @param total_tiles_columns The total number of tiles within column
+ * @param tile_width The tile width
  * @param gamma SoftDTW smoothing parameter
  */
-
-
-
 
 __global__ void softdtw_global_tiled(float *da, float *db, float *D, int waveId,
                                      uint total_tiles_rows,
@@ -64,9 +50,9 @@ __global__ void softdtw_global_tiled(float *da, float *db, float *D, int waveId,
  * @param n Length of second time series
  * @param D The pairwise distance array of two time series
  * @param waveId the ID for the wave
- * @param total_tiles_rows
- * @param total_tiles_columns
- * @param tile_width
+ * @param total_tiles_rows The total number of tiles within the row
+ * @param total_tiles_columns The total number of tiles within the column
+ * @param tile_width  The tile width
  * @param tileRow
  * @param tileColumn
  * @param gamma SoftDTW smoothing parameter
