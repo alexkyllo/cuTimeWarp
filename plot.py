db3a0dd7e2951a8509d270ac2c23240e7f1ea1e1
Alex Kyllo <alex.kyllo@gmail.com>
2021-03-18 23:39:13 -0700

    add profiler table for 1000 length

--- plot.py
+++ plot.py
@@ -270,49 +270,68 @@ plot_cpu_gpu(
 
 # Profiling data
 
-prof_kernels = {
-    "softdtw_naive_kernel_multi": "1 naive",
-    "softdtw_stencil": "3 stencil",
-    "softdtw_diagonal_kernel_multi": "2 diagonal",
-    # "soft_dtw_tiled_multi": "tiled",
-}
 
-metrics = {
-    "Achieved Occupancy": "Occupancy",
-    "Registers Per Thread": "Registers / Thread",
-    "L1/TEX Hit Rate": "L1 Cache Hit",
-    "L2 Hit Rate": "L2 Cache Hit",
-    "SM Busy": "SM Busy",
-    "Mem Busy": "Mem Busy",
-}
+def profile_data(infile, outfile, caption, label):
+    prof_kernels = {
+        "softdtw_naive_kernel_multi": "1 naive",
+        "softdtw_stencil": "3 stencil",
+        "softdtw_diagonal_kernel_multi": "2 diagonal",
+    }
 
-df_prof = pd.read_csv("output/ncu_100_2.csv")
+    metrics = {
+        "Achieved Occupancy": "Occupancy",
+        "Registers Per Thread": "Registers / Thread",
+        "L1/TEX Hit Rate": "L1 Cache Hit",
+        "L2 Hit Rate": "L2 Cache Hit",
+        "SM Busy": "SM Busy",
+        "Mem Busy": "Mem Busy",
+    }
 
-df_prof["Kernel"] = df_prof["Kernel Name"].str.split("(", n=1, expand=True)[0]
+    df_prof = pd.read_csv(infile)
 
-df_prof = df_prof[
-    (df_prof["Kernel"].isin(prof_kernels))
-    & (df_prof["Metric Name"].isin(metrics))
-]
+    df_prof["Kernel"] = df_prof["Kernel Name"].str.split("(", n=1, expand=True)[
+        0
+    ]
 
-df_prof["Kernel"] = df_prof["Kernel"].apply(lambda x: prof_kernels[x])
-df_prof["Metric"] = df_prof["Metric Name"].apply(lambda x: metrics[x])
+    df_prof = df_prof[
+        (df_prof["Kernel"].isin(prof_kernels))
+        & (df_prof["Metric Name"].isin(metrics))
+    ]
 
-df_prof["Metric Value"] = df_prof["Metric Value"].astype(float)
-df_prof_sum = (
-    df_prof.groupby(["Kernel", "Metric"])["Metric Value"]
-    .mean()
-    .round(2)
-    .reset_index()
-    .pivot_table(index="Kernel", columns=["Metric"])
-    .reset_index()
+    df_prof["Kernel"] = df_prof["Kernel"].apply(lambda x: prof_kernels[x])
+    df_prof["Metric"] = df_prof["Metric Name"].apply(lambda x: metrics[x])
+
+    df_prof["Metric Value"] = df_prof["Metric Value"].astype(float)
+    df_prof_sum = (
+        df_prof.groupby(["Kernel", "Metric"])["Metric Value"]
+        .mean()
+        .round(2)
+        .reset_index()
+        .pivot_table(index="Kernel", columns=["Metric"])
+        .reset_index()
+    )
+
+    df_prof_sum.to_latex(
+        outfile,
+        index=False,
+        caption=caption,
+        label=label,
+        position="htbp",
+    )
+
+
+profile_data(
+    "output/ncu_100_2.csv",
+    "fig/prof_table.tex",
+    caption="NVIDIA NSight Compute Profiler metrics by kernel (length 100)",
+    label="prof_table",
 )
 
-df_prof_sum.to_latex(
-    "fig/prof_table.tex",
-    index=False,
-    caption="NVIDIA NSight Compute Profiler metrics by kernel",
-    label="prof_table",
+profile_data(
+    "output/ncu_1000_2.csv",
+    "fig/prof_table_1000.tex",
+    caption="NVIDIA NSight Compute Profiler metrics by kernel (length 1000)",
+    label="prof_table_1000",
 )
 
 # ECG perf
@@ -391,4 +410,5 @@ df_ecg.to_latex(
     index=False,
     caption="Performance by kernel on the ECG200 dataset.",
     label="ecg_table",
+    position="htbp",
 )
